a7fd95b55407177d6e2f893cfccae6a9c019bb06
Agor Maxime <maxime.agor23@gmail.com>
2016-05-09 21:01:16 +0200

    uses express redirections instead of sendFile

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
 
-// lit les fichiers HTML a envoyer aux clients
+// lit les fichiers HTML à envoyer aux clients
 var path = require('path');
 var fs = require('fs');
 var publicFolder = path.join(__dirname, 'public');
-var phoneHTML = path.join(publicFolder, 'phone.html');
-var webHTML = path.join(publicFolder, 'web.html');
 
 // permet de detecter si PC ou mobile
 var device = require('express-device');
@@ -23,10 +21,13 @@ app.use(express.static(publicFolder));
 // si on se connecte sur www.monsite.com/
 app.get('/', function(req, res) {
 	// verifie le type d'appareil
-	if (req.device.type === 'tablet' || req.device.type === 'phone')
-		res.sendFile(phoneHTML);
-	else
-		res.sendFile(webHTML);
+	if (req.device.type === 'tablet' || req.device.type === 'phone') {
+		res.redirect('phone.html');
+	}
+
+	else {
+		res.redirect('web.html');
+	}
 });
 
 // quand une connexion socket.io (communication temps réel) arrive
@@ -35,7 +36,7 @@ io.on('connection', function(socket)
 	// quand un message de type 'gyro' arrrive
 	socket.on('gyro', function(data) {
 		// le renvoie à tous les autres clients connectés
-		// (faudra le changer, avec le systeme de code de dadagafa pour les faire marche par paire)
+		// (faudra le changer, avec le systeme de code de dadagafa pour les faire marcher par paire)
 		socket.broadcast.emit('gyro', data);
 	});
 })
